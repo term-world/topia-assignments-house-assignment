b6329be29b9b639a88ae6fac9ff77dc74d35f512
normile01@allegheny.edu <normile01@allegheny.edu>
2022-07-22 18:13:43 +0000

    Reduce characters per line in paths.yml

--- living-room/note-from-landlord.py
+++ living-room/note-from-landlord.py
@@ -1,9 +1,43 @@
 import narrator
+from narrator import Checkpoint
 
 n = narrator.Narrator()
+#ref: n.path.change()
+
+
+def binary_ask(question: str, yes_path: float):
+    q = narrator.Question({
+        "question": question,
+        "responses": [
+            {"choice": "yes", "outcome": yes_path},
+            {"choice": "no", "outcome": 0.5}
+        ]})
+    while True:
+        ask = input(q.prompt)
+        if ask in q.responses:
+            path = q.responses[ask].outcome
+            n.path.change(path)
+            break
+        print("Not a valid option; try again.")
 
 def main():
-    flag = narrator.Checkpoint.set_flag("boxes_unpacked", 2)
+    if Checkpoint.check_flag("boxes_unpacked") == 5 and bool(Checkpoint.check_flag("lease_printed")):
+        # TO-DO: Narrate the prompt to delete the note, and delete the note
+    if bool(Checkpoint.check_flag("note_read")):
+        print("yo")
+    else:
+        n.narrate()
+        binary_ask("Read the note?", 0.1)
+        n.narrate()
+        if n.path.scene != 6:
+            binary_ask("Continue reading?", 0.2)
+            n.narrate()
+            if n.path.scene != 6:
+                binary_ask("Continue reading?", 0.3)
+                n.narrate()
+                if n.path.scene != 6:
+                    binary_ask("Continue reading?", 0.4)
+                    n.narrate()
 
 
 if __name__ == "__main__":
